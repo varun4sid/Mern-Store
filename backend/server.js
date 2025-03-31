4417faa4d2197d4db5f82b5c21de2dd281eb8fc3
varun <varun4socials@gmail.com>
2025-03-31 11:22:25 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import express from "express";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import productsRouter from "./routes/products.route.js";
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use("/api/products", productsRouter);
 
-const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server started at http://localhost:${PORT}`);
